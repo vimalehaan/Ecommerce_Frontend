1b5381209637bb14fbfd0fa25a40a2327e33e134
Thanakarn <rasenthiramthana2399@gmail.com>
2024-11-21 16:41:00 +0530

    first commit

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -6,8 +6,41 @@ import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 
 import NavBar from "../Components/Utils/NavBar";
+import WishListCard from "../Components/WishLishCard/WishListCard";
+
 
 const HomePage = () => {
+  const products = [
+    {
+      id: 1,
+      image: 'https://via.placeholder.com/100',
+      title: 'T-shirt with multiple colors',
+      details: 'Size: medium, Color: blue, Material: Plastic\nSeller: Artel Market',
+      price: 39.0,
+    },
+    {
+      id: 2,
+      image: 'https://via.placeholder.com/100',
+      title: 'Winter Jacket',
+      details: 'Size: large, Color: brown, Material: Wool\nSeller: Winter Wear Co.',
+      price: 79.0,
+    },
+    {
+      id: 3,
+      image: 'https://via.placeholder.com/100',
+      title: 'Denim Shorts',
+      details: 'Size: small, Color: black, Material: Denim\nSeller: Artel Market',
+      price: 29.0,
+    },
+  ];
+
+  const handleDelete = (id) => {
+    console.log(`Delete item with id: ${id}`);
+  };
+
+  const handleAddToCart = (id) => {
+    console.log(`Add to cart item with id: ${id}`);
+  };
   return (
     <div>
       <Box
@@ -25,6 +58,19 @@ const HomePage = () => {
           }}
         >
           <NavBar />
+          <div>
+      {products.map((product) => (
+        <WishListCard
+          key={product.id}
+          image={product.image}
+          title={product.title}
+          details={product.details}
+          price={product.price}
+          onDelete={() => handleDelete(product.id)}
+          onAddToCart={() => handleAddToCart(product.id)}
+        />
+      ))}
+    </div>
         </Box>
       </Box>
       {/* <Container>
